905d53bbf96f31e6cd97b8ee010126a2f64107cc
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-02-08 13:25:36 +0330

    fix logo to left for mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
   return (
     <nav className="bg-purple-500">
       <div className="flex items-center font-medium justify-around">
-        <div>
+        <div className="z-50 p-5 md:w-auto w-full">
           <img src="" alt="logo" className="md:cursor-pointer" />
         </div>
         <ul className="md:flex hidden  uppercase items-center gap-8 font-medium">
