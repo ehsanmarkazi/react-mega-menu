e3e9e51f0870f25e5ccc8247b0a1ac7d19d45007
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-02-08 12:40:53 +0330

    add style & text

--- src/components/Navbar/Button.tsx
+++ src/components/Navbar/Button.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const Button = () => {
-  return <button className='bg-primary'>
-
+  return <button className='bg-slate-800 text-white px-6 py-2 rounded-full'>
+    Get Start
   </button>;
 }
 
