f81bfdce1c7b9091512c34cb5e4fa8e4a40ea174
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-02-09 15:01:01 +0330

    create for mobile

--- src/components/Navbar/NavLinks.tsx
+++ src/components/Navbar/NavLinks.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { links } from "./myLinks";
+import links from "./myLinks";
 
 const NavLinks = () => {
-  
+  const [heading, setHeading] = useState("");
+  const [subHeading, setSubHeading] = useState("");
   return (
     <>
       {links.map((link) => (
         <div>
-          <div className="px-3 test-left md:cursor-pointer group">
-            <h1 className="py-7">{link.name}</h1>
+          <div className="px-3 text-left md:cursor-pointer group">
+            <h1
+              className="py-7 "
+              onClick={() => {
+                heading !== link.name ? setHeading(link.name) : setHeading("");
+                setSubHeading("");
+              }}
+            >
+              {link.name}
+              <span>
+                
+              </span>
+            </h1>
+
             {link.submenu && (
               <div>
-                <div className="absolute top-20 hidden group-hover:block hover:block">
+                <div className="absolute top-20 hidden group-hover:md:block hover:md:block">
                   <div className="py-3">
                     <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45 "></div>
                   </div>
@@ -38,6 +51,39 @@ const NavLinks = () => {
                 </div>
               </div>
             )}
+          </div>
+          {/* Mobile Menus */}
+          <div
+            className={`${heading === link.name ? "md:hidden " : "hidden"} `}
+          >
+            {/* sublinks */}
+            {link.subLinks.map((slinks) => (
+              <div>
+                <div>
+                  <h1
+                    onClick={() =>
+                      subHeading !== slinks.Head
+                        ? setSubHeading(slinks.Head)
+                        : setSubHeading("")
+                    }
+                    className="py-4 pl-4 font-semibold md:pr-0 pr-5"
+                  >
+                    {slinks.Head}
+                  </h1>
+                  <div
+                    className={`${
+                      subHeading === slinks.Head ? "md:hidden" : "hidden"
+                    }`}
+                  >
+                    {slinks.subLink.map((slink) => (
+                      <li className="py-3 pl-14">
+                        <Link to={slink.link}>{slink.name}</Link>
+                      </li>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       ))}
