ee81cc749526e6c41538a3733bb463f489fc3be3
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-02-09 18:24:41 +0330

    refactor app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import Button from "./components/Navbar/Button";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -10,16 +9,8 @@ function App() {
     font-[Poppins] md:bg-top bg-center
     "
     >
-      <Navbar/>
-      <div className="flex flex-col justify-center text-center items-center h-full bottom-0 bg-slate-800">
-      <h2 className="text-black text-2xl font-medium">Fashion Tips</h2>
-      <h1 className="md:text-5xl text-3xl text-black font-semibold py-5">
-        Items every woman should have
-      </h1>
-      <div className="text-x1">
-        <Button/>
-      </div>
-      </div>
+      <Navbar />
+      <div className="flex flex-col justify-center text-center items-center h-full bottom-0 bg-slate-800"></div>
     </section>
   );
 }
