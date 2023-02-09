d32e5ef52ff725457396c47cb49bdb8fed240de9
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-02-09 10:05:05 +0330

    create maps & menus

--- src/components/Navbar/NavLinks.tsx
+++ src/components/Navbar/NavLinks.tsx
@@ -1,41 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { links } from "./myLinks";
 
 const NavLinks = () => {
-  const links = [
-    {
-      name: "Men",
-      submenu: true,
-      subLinks: [
-        {
-          Head: "Topwear",
-          subLink: [
-            { name: "T-shirt", link: "/" },
-            { name: "Casual shirt", link: "/" },
-            { name: "Formal shirt", link: "/" },
-            { name: "Formal shirt", link: "/" },
-            { name: "Formal shirt", link: "/" },
-          ],
-        },
-      ],
-    },
-    { name: "Women" },
-    { name: "Kid's" },
-  ];
+  
   return (
     <>
       {links.map((link) => (
         <div>
-          <div className="px-3 test-left md:cursor-pointer">
+          <div className="px-3 test-left md:cursor-pointer group">
             <h1 className="py-7">{link.name}</h1>
             {link.submenu && (
               <div>
-                <div>
-                  <div>
+                <div className="absolute top-20 hidden group-hover:block hover:block">
+                  <div className="py-3">
+                    <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45 "></div>
+                  </div>
+                  <div className="bg-white p-5 grid grid-cols-3 gap-10">
                     {link.subLinks.map((mySubLink) => (
                       <div>
-                        <h1>{mySubLink.Head}</h1>
-                        {mySubLink.subLink.map(sLink=>(
-                          <li>{sLink.name}</li>
+                        <h1 className="text-lg font-semibold">
+                          {mySubLink.Head}
+                        </h1>
+                        {mySubLink.subLink.map((sLink) => (
+                          <li className="text-sm text-gray-600 my-3">
+                            <Link
+                              to={sLink.link}
+                              className="hover:text-primary"
+                            >
+                              {sLink.name}
+                            </Link>
+                          </li>
                         ))}
                       </div>
                     ))}
