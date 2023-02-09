0da9b53c990e85ef2d9becdb97158ca0419a7413
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-02-09 10:04:00 +0330

    add sub menu

--- src/components/Navbar/NavLinks.tsx
+++ src/components/Navbar/NavLinks.tsx
@@ -1,13 +1,48 @@
 import React from "react";
 
 const NavLinks = () => {
-  const links = [{ name: "Men" }, { name: "Women" }, { name: "Kid's" }];
+  const links = [
+    {
+      name: "Men",
+      submenu: true,
+      subLinks: [
+        {
+          Head: "Topwear",
+          subLink: [
+            { name: "T-shirt", link: "/" },
+            { name: "Casual shirt", link: "/" },
+            { name: "Formal shirt", link: "/" },
+            { name: "Formal shirt", link: "/" },
+            { name: "Formal shirt", link: "/" },
+          ],
+        },
+      ],
+    },
+    { name: "Women" },
+    { name: "Kid's" },
+  ];
   return (
     <>
       {links.map((link) => (
         <div>
           <div className="px-3 test-left md:cursor-pointer">
             <h1 className="py-7">{link.name}</h1>
+            {link.submenu && (
+              <div>
+                <div>
+                  <div>
+                    {link.subLinks.map((mySubLink) => (
+                      <div>
+                        <h1>{mySubLink.Head}</h1>
+                        {mySubLink.subLink.map(sLink=>(
+                          <li>{sLink.name}</li>
+                        ))}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       ))}
